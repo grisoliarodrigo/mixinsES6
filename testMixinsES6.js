0058b04c1ea95208debd5b37dbd1069785166e50
grisoliarodrigo <grisoliarodrigo@gmail.com>
2015-12-15 16:10:19 -0300

    -

---
+++ testMixinsES6.js
@@ -0,0 +1,61 @@
+var enableDisable = {
+  activar: function() {
+    this.activo = true;
+  },
+
+  desActivar: function() {
+    this.activo = false;
+  }
+};
+
+var io = {
+  toString: function() {
+    return this.descripcion + " - Activo: " + this.activo;
+  }
+
+  superFuncion: function() {
+    return "superFuncionDelMixin"
+  }
+};
+
+class superClase {
+
+  constructor() {
+    this.superProperty = "a"
+  }
+
+  superFunction() {
+    return "superFuncion"
+  }
+
+}
+
+class unaClase extends superClase {
+
+  constructor(descripcion) {
+    super()
+    mixin(this, enableDisable, io);
+  	this.descripcion = descripcion;
+  }
+
+}
+
+function mixin(objeto, ...mixins) {
+  mixins.forEach( it => bindMixin(objeto, it))
+}
+
+function bindMixin(objeto, mixin) {
+  for (x in mixin) {
+    objeto[x] = mixin[x];
+  }
+}
+
+function test() {
+  var unObjeto = new unaClase("objeto");
+  console.log(unObjeto.superFunction());
+  console.log(unObjeto.toString());
+  unObjeto.activar();
+  console.log(unObjeto.toString());
+  unObjeto.desActivar();
+  console.log(unObjeto.toString());
+}
